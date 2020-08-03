186b936f72c0a899a63952227808d9cbb520242e
Hemarloved <62019042+Hemarloved@users.noreply.github.com>
2020-08-03 06:26:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    grabCursor:true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
